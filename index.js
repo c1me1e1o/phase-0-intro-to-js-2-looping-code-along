a82a0a013a74dac101bf0bafb97b9789d68821e5
Carlmelo <car1melo@hotmail.com>
2023-03-11 12:18:04 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const writeCards = (name, event) => {
+    const messages = []
+    for(let i = 0; i < name.length;i++) {
+        
+           const greetingMessage = `Thank you, ${name[i]}, for the wonderful ${event} gift!`
+           messages.push(greetingMessage)
+    }
+    return messages
+}
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"))
+
+
+function countDown() {
+    countDown = 10
+    while (countDown > -1) {
+    console.log(countDown--);
+   }
+}
